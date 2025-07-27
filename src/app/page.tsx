2f060f4c3bd124b907b6f5286fde3c73be6afd46
Mr.chu <chuhongkyu@gmail.com>
2025-07-27 19:20:07 +0900

    Fixed WayPoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,20 @@ import Bottom from "@/components/Bottom";
 export default function Home() {
   return (
     <div className="h-[100dvh] p-3 overflow-hidden bg-gray-200">
-      <div className="grid grid-cols-5 grid-rows-5 h-full gap-3">
-        <main className="col-span-4 row-span-4 bg-white rounded-lg overflow-hidden">
-          <Scene />
-        </main>
-        <aside className="col-span-1 row-span-4 bg-white rounded-lg overflow-hidden px-4">
-          <Panel />
-        </aside>
-        <footer className="col-span-5 row-span-1 bg-white rounded-lg overflow-hidden">
+      <main className="w-full h-[calc(100%-150px)]">
+        <div className="flex gap-3 w-full h-full">
+          <section className="w-[calc(100%-320px)] bg-white rounded-lg overflow-hidden">
+            <Scene />
+          </section>
+          <aside className="w-[310px] bg-white rounded-lg overflow-hidden px-4">
+            <Panel />
+          </aside>
+        </div>
+    
+        <footer className="min-h-[140px] mt-3 bg-white rounded-lg overflow-hidden">
           <Bottom />
         </footer>
-      </div>
+      </main>
     </div>
   );
 }
